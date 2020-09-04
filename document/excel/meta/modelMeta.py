fca684f1c349ac236434aebd08facb9ef4d9cdaf
Tony.Don <yeihizhi@163.com>
2020-09-05 07:44:27 +0800

    document/excel/meta/modelMeta.py 所有数据加载进内存处理. threadPools.py 增加装饰器 PoolsFunc. threadPools.py fix threadPools bug.

--- document/excel/meta/modelMeta.py
+++ document/excel/meta/modelMeta.py
@@ -5,6 +5,7 @@ from openpyxl import load_workbook
 import csv
 import copy
 
+
 class _AttributeOverride:
     def __init__(self, name, m_type):
         self._name = name
@@ -19,16 +20,17 @@ class _AttributeOverride:
     def __delete__(self, instance):
         instance.__dict__.pop(self._name)
 
+
 class Meta:
     file = None
     sheet = 'sheet1'
-    extension = 'xlsx' 
+    extension = 'xlsx'
 
 
 class modelMeta(type):
     def __new__(cls, name, bases, dct):
-        attrs = {'mapping': {}, 'Meta':Meta, '__exit__': __exit__, 'readLine': readLine,
-                 'close': close, '__initExcelHandle__': __initExcelHandle__}
+        attrs = {'mapping': {}, 'Meta': Meta, '__exit__': __exit__, 'readLine': readLine,
+                 'close': close, '__initExcelHandle__': __initExcelHandle__, 'rows': rows, 'getCount': getCount, '_translateMode': _translateMode, '__rowsCount__': 0}
         for name, value in dct.items():
             if (not isinstance(dct[name], type)) and (not isinstance(dct[name], FunctionType)):
                 if not name.startswith('__'):
@@ -37,35 +39,89 @@ class modelMeta(type):
                         attrs[name] = _AttributeOverride(name, value.fieldType)
                     else:
                         raise Exception('field type must is FieldType.')
+                else:
+                    attrs[name] = value
             else:
                 if isinstance(dct[name], type):
                     if name == 'Meta':
                         for attr_name in dct[name].__dict__:
                             if not attr_name.startswith('__'):
-                                setattr(attrs['Meta'], attr_name, dct[name].__dict__[attr_name])
+                                setattr(attrs['Meta'], attr_name,
+                                        dct[name].__dict__[attr_name])
                 else:
                     attrs[name] = value
         return super(modelMeta, cls).__new__(cls, name, bases, attrs)
 
 
-
-
 def __initExcelHandle__(self):
     try:
         if self.Meta.file:
-            m_extension= self.Meta.file.rsplit('.')[1]
-            if m_extension== 'csv':
-                self.Meta.extension= 'csv'
-                self.__excelHandle__= open(self.Meta.file)
-                self.__sheetHandle__= csv.reader(self.__excelHandle__)
-            else:
-                self.Meta.extension= 'xlsx'
+            m_extension = self.Meta.file.rsplit('.')[1]
+            if m_extension == 'csv':
+                self.Meta.extension = 'csv'
+                self.__excelHandle__ = open(self.Meta.file)
+                self.__sheetHandle__ = csv.reader(self.__excelHandle__)
+                self.__excelHandle__ = None
+                m_offset = 1
+                for v in self.__sheetHandle__:
+                    if v:
+                        if m_offset < self.__record_offset__:
+                            m_offset += 1
+                            continue
+                        self.__rows__.append(self._translateMode(v))
+                    else:
+                        break
+                self.__rowsCount__ = len(self.__rows__)
+            elif m_extension == 'xlsx' or m_extension == 'xls':
+                self.Meta.extension = 'xlsx'
                 self.__excelHandle__ = load_workbook(self.Meta.file)
                 self.__sheetHandle__ = self.__excelHandle__[self.Meta.sheet]
+                column = self.__sheetHandle__.max_column+1
+                m_offset= 1
+                tmp_handle= self.__sheetHandle__
+                for m_rows in tmp_handle:
+                    if m_offset < self.__record_offset__:
+                        m_offset += 1
+                        continue
+                    self.__rows__.append(self._translateMode(m_rows))
+                self.__rowsCount__ = len(self.__rows__)
+                tmp_handle= None
+            else:
+                raise Exception('file extension is error.')
         else:
             raise Exception('meta file is None.')
     except Exception as e:
         raise e
+
+
+def _translateMode(self, data=[]):
+    m_extension = self.Meta.file.rsplit('.')[1].lower()
+    if data:
+        try:
+            # 关闭IO，然后返回深拷贝对象解决数组里对象值引用引起的所有对象同样的值.
+            # (没搞明白，为什么这里设为NONE了。外层的for循环还能继续下去)
+            self.__sheetHandle__ = None
+            self.__excelHandle__ = None
+            for (name, value) in self.mapping.items():
+                if value <= len(data):
+                    if m_extension == 'csv':
+                        attr_value = data[value-1]
+                    elif m_extension == 'xlsx' or m_extension == 'xls':
+                        attr_value= data[value-1].value
+                    else:
+                        raise Exception('file extension is error.')
+                    if not attr_value:
+                        attr_value = '0.00'
+                    if attr_value == 'None':
+                        attr_value = '0.00'
+                    setattr(self, name, attr_value)
+                else:
+                    raise Exception('mapping error:(%s,%s)' % (name, value))
+            return copy.deepcopy(self)
+        except Exception as e:
+            raise e
+    else:
+        raise Exception("translate data is none.")
 
 
 def __exit__(self, exc_type, exc_value, exc_t):
@@ -77,44 +133,16 @@ def close(self):
     self.__sheetHandle__ = None
 
 
-def readLine(self, lineOffset=1):
-    if lineOffset < 1:
-        lineOffset = 1
-    if self.__sheetHandle__:
-        if self.Meta.extension == 'xlsx':
-            if lineOffset <= self.__sheetHandle__.max_row:
-                rowdata = []
-                column = self.__sheetHandle__.max_column+1
-                for i in range(1, column):
-                    cellvalue = self.__sheetHandle__.cell(
-                        row=lineOffset, column=i).value
-                    rowdata.append(cellvalue)
-                for (name, value) in self.mapping.items():
-                    if value <= len(rowdata):
-                        setattr(self, name, rowdata[value-1])
-                    else:
-                        raise Exception('mapping error:(%s,%s)' % (name, value))
-                return self
-            else:
-                return None
-        else:
-            rowdata= []
-            for offset in range(0,lineOffset):
-                try:
-                    rowdata= next(self.__sheetHandle__)
-                except Exception as e:
-                    rowdata=[]
-            if rowdata:
-                for (name, value) in self.mapping.items():
-                    if value <= len(rowdata):
-                        attr_value=rowdata[value-1]
-                        if attr_value=='None':
-                            attr_value='0.00'
-                        setattr(self, name, attr_value)
-                    else:
-                        raise Exception('mapping error:(%s,%s)' % (name, value))
-                return self
-            else:
-                return None
-    else:
-        raise Exception('Sheet Handle is None.')
+def getCount(self):
+    return self.__rowsCount__
+
+
+def rows(self):
+    return self.__rows__
+
+
+def readLine(self, lineOffset=0):
+    if lineOffset >= self.getCount():
+        return None
+    self.__read_offset__= lineOffset
+    return self.__rows__[lineOffset]
